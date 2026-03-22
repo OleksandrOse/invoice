16d6a233c4649c77f3562df4f925a3d82430530f
Oleksandr Oselskyi <oselskyi3@gmail.com>
2026-03-22 19:20:38 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ const App: React.FC = () => {
   } = useInvoiceForm();
 
   const handlePrint = () => { printInvoice(form, totals); };
-
   return (
     <div className="app">
       <div className="formPanel">
